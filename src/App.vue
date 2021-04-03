fe1f1bd20ffc144853e0db1ac65b1dc3a8153b9f
SamHo <sam.ho@kdanmobile.com>
2021-04-03 21:59:37 +0800

    Add a home page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
+    <h2 id="title">RS's CSS Practice</h2>
+    <hr id="title-divider">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/todo">Todo</router-link>
+      <router-link class="nav-item" to="/">Home</router-link>
+      <router-link class="nav-item" to="/todo">Todo</router-link>
     </div>
+    <hr id="nav-divider">
     <router-view/>
   </div>
 </template>
@@ -18,7 +21,8 @@
 }
 
 #nav {
-  padding: 0px;
+  padding: 10px;
+  margin: 10px;
 }
 
 #nav a {
@@ -28,5 +32,33 @@
 
 #nav a.router-link-exact-active {
   color: #2c3e50;
+}
+
+#title {
+  margin: 30px;
+}
+
+#title-divider {
+  border-width: 4px;
+  border-color: #123456;
+  opacity: 80%;
+  width: 80%;
+  border-radius: 10px;
+}
+
+#nav-divider {
+  border-width: 1px;
+  border-color: #123456;
+  opacity: 50%;
+  width: 80%;
+  border-radius: 10px;
+}
+
+.nav-item {
+  margin: 20px;
+}
+
+router-link {
+
 }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>HomePage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+p {
+  display: inline;
+  border: 1px solid #123456;
+  margin: 10px 10px 10px 10px;
+  padding: 10px;
+  box-shadow: 3px 5px 10px 1px;
+}
+</style>
